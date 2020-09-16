fb1fb7534ca8b213aee96541b719bb896124c314
Mychal.Smith <mychsmit@gmail.com>
2020-09-16 09:06:38 -0600

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Mychal Smith on 9/16/20.
+//  Copyright © 2020 Mychal Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
